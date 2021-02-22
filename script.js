62635b7ef4a596d488fc810e59641ca746fc53ca
Ideraldo <67620423+Ideraldo@users.noreply.github.com>
2021-02-22 20:01:46 -0300

    Play button
    Botão de "play" adicionado

--- script.js
+++ script.js
@@ -1,71 +1,102 @@
-const dino = document.querySelector('.dino')
-const background = document.querySelector('.background')
-let isJumping = false;
-let position = 0
+function startGame() {
 
-//Checks whether the user is pressing the space bar
-function handleKeyUp(event) {
-    if (event.keyCode === 32) {
-        if (!isJumping) {
+    // const dino = document.querySelector('.dino')
+    // const background = document.querySelector('.background')
+    let isJumping = false;
+    let position = 0
+
+    //Cria div dino e o background
+    const background = document.createElement("div")
+    
+
+
+    //Changes dino's and background's opacities
+    function changeOpacity() {
+        if (dino.style.opacity == 0 && background.style.opacity == 0) {
+            dino.style.opacity = 1
+            background.style.opacity = 1
+        }
+    }
+    changeOpacity()
+
+
+    //Checks whether the user is pressing the space bar
+    function handleKeyDown(event) {
+        if (event.keyCode === 32 && !isJumping) {
             jump()
         }
     }
-}
 
-function jump() {
+    function jump() {
 
-    isJumping = true;
+        isJumping = true;
 
-    let upInterval = setInterval(function () {
-        if (position >= 150) {
-            clearInterval(upInterval)
+        let upInterval = setInterval(function () {
+            if (position >= 150) {
+                clearInterval(upInterval)
 
-            //Down
-            let downInterval = setInterval(function () {
-                if (position <= 0) {
-                    clearInterval(downInterval)
-                    isJumping = false;
-                } else {
-                    position -= 20
-                    dino.style.bottom = position + 'px'
-                }
-            }, 20)
-        } else {
-            //Up
-            position += 20
-            dino.style.bottom = position + 'px'
-        }
-    }, 20)
-}
+                //Down
+                let downInterval = setInterval(function () {
+                    if (position <= 0) {
+                        clearInterval(downInterval)
+                        isJumping = false;
+                    } else {
+                        position -= 20
+                        dino.style.bottom = position + 'px'
+                    }
+                }, 20)
+            } else {
+                //Up
+                position += 20
+                dino.style.bottom = position + 'px'
+            }
+        }, 20)
+    }
 
+    function createCactus() {
 
-
-function createCactus() {
-    const cactus = document.createElement('div')
-    let cactusPosition = 1000
-    let randomTime = Math.random() * 6000
-
-    cactus.classList.add('cactus')
-    cactus.style.left = 1000 + 'px'
-    background.appendChild(cactus)
-
-    let leftInterval = setInterval(function () {
-        if (cactusPosition < -60) {
+        function gameOver() {
             clearInterval(leftInterval)
             background.removeChild(cactus)
-        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
-            //Game over
-            clearInterval(leftInterval)
-           document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>'
-        } else {
-            cactusPosition -= 10;
-            cactus.style.left = cactusPosition + 'px'
+            document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>'
+            //Criar botao de play again
+            function createButton() {
+                var btn = document.createElement('BUTTON')
+                var lbl = document.createTextNode("PLAY AGAIN")
+                btn.appendChild(lbl)
+                btn.onclick = startGame()
+                document.body.appendChild(btn);
+            }
+            createButton()
+
         }
 
-    }, 20)
+        const cactus = document.createElement('div')
+        let cactusPosition = 1000
+        let randomTime = Math.random() * 6000
 
-    setTimeout(createCactus, randomTime)
+        cactus.classList.add('cactus')
+        cactus.style.left = 1000 + 'px'
+        background.appendChild(cactus)
+
+        let leftInterval = setInterval(function () {
+            if (cactusPosition < -60) {
+                clearInterval(leftInterval)
+                background.removeChild(cactus)
+            } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+                gameOver()
+            } else {
+                cactusPosition -= 10;
+                cactus.style.left = cactusPosition + 'px'
+            }
+
+        }, 20)
+
+        setTimeout(createCactus, randomTime)
+    }
+
+    createCactus();
+    document.addEventListener('keyup', handleKeyDown)
 }
 
-createCactus();
-document.addEventListener('keyup', handleKeyUp)
+// startGame()
